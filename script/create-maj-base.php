f854c130275c6f0d087ed7c27b8644412b752b25
Gauthier <gauthier.verdol@atm-consulting.fr>
2014-05-15 14:35:32 +0200

    debug

--- script/create-maj-base.php
+++ script/create-maj-base.php
@@ -2,13 +2,13 @@
 /*
  * Script créant et vérifiant que les champs requis s'ajoutent bien
  */
-define('INC_FROM_CRON_SCRIPT', true);
+define('INC_FROM_DOLIBARR', true);
 
 dol_include_once('/declinaison/config.php');
 dol_include_once('/declinaison/class/declinaison.class.php');
 
 $PDOdb=new TPDOdb;
-$PDOdb->db->debug=true;
+//$PDOdb->db->debug=true;
 
 $o=new TDeclinaison;
 $o->init_db_by_vars($PDOdb);
